4c0c6e3d3d7569ee4cfd9ce69c7e579d40cd2c04
Abhijeet Gupta <53190754+abhijeetgupto@users.noreply.github.com>
2021-10-27 17:13:47 +0530

    Time: 28 ms (81.72%), Space: 14.1 MB (90.62%) - LeetHub

---
+++ n-th-tribonacci-number/n-th-tribonacci-number.py
@@ -0,0 +1,9 @@
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        
+        dp = [0,1,1]
+        while len(dp) <= n :
+            dp.append(dp[-1]+dp[-2]+dp[-3])
+        
+        return dp[n]
+        
