a47fd450e5b42adb1714bb62e540fa35ae74b138
Abhijeet Gupta <53190754+abhijeetgupto@users.noreply.github.com>
2021-10-27 17:11:52 +0530

    Time: 28 ms (88.70%), Space: 14.2 MB (41.61%) - LeetHub

---
+++ fibonacci-number/fibonacci-number.py
@@ -0,0 +1,12 @@
+class Solution:
+    def fib(self, n: int) -> int:
+        
+        if n == 0 :
+            return 0
+        
+        dp = [0,1]
+        while len(dp) < n+1 :
+            dp.append(dp[-1]+dp[-2])
+        
+        return dp[-1]
+        
