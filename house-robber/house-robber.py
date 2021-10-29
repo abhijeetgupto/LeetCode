5da5eaff104d9e08ead5798d641de2d2898cc49e
Abhijeet Gupta <53190754+abhijeetgupto@users.noreply.github.com>
2021-10-29 14:56:19 +0530

    Time: 32 ms (75.86%), Space: 14.4 MB (19.75%) - LeetHub

--- house-robber/house-robber.py
+++ house-robber/house-robber.py
@@ -1,21 +1,17 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         
-        if len(nums)==1:
-            return nums[0]
-        
-        if len(nums)==2:
+        if len(nums) <=  2 :
             return max(nums)
         
         if len(nums) == 3 :
             return max(nums[1],nums[0]+nums[2])
         
         dp = [0]*len(nums)
-        dp[0], dp[1] = nums[0], nums[1]
-        for i in range(2,len(nums)):
-            try :
-                dp[i] = max(nums[i]+dp[i-2],dp[i-1],nums[i]+dp[i-3])
-            except:
-                 dp[i] = max(nums[i]+dp[i-2],dp[i-1],nums[i])
-                
+        dp[0],dp[1],dp[2] = nums[0],nums[1],max(nums[2]+nums[0],nums[1])
+        
+        
+        for i in range(3, len(dp)):
+            dp[i] = max(dp[i-1], nums[i]+dp[i-2], nums[i]+dp[i-3])
+            
         return dp[-1]
