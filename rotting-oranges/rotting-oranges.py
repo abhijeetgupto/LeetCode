f708546a90f92586f01735af631f3d0b6d8fe6de
Abhijeet Gupta <53190754+abhijeetgupto@users.noreply.github.com>
2021-10-29 14:15:46 +0530

    Time: 56 ms (60.35%), Space: 14.5 MB (9.75%) - LeetHub

--- rotting-oranges/rotting-oranges.py
+++ rotting-oranges/rotting-oranges.py
@@ -1,46 +1,35 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         
+        good = 0
         queue = []
-        count = 0
-        
         for i in range(len(grid)):
             for j in range(len(grid[0])):
-                if grid[i][j] == 2:
+                if grid[i][j] == 1 :
+                    good += 1
+                elif grid[i][j] == 2 :
                     queue.append([i,j])
-                elif grid[i][j] == 1:
-                    count += 1
-        
-        if count == 0 :
-            return 0
-        
-        time = 0
-        while queue and count>0 :
-            time += 1
-            nq = []
-            
-            def helper(row,col) :
-                nonlocal count
-                traverse = [(row,col-1),(row,col+1),(row-1,col),(row+1,col)]
-                for l in traverse :
-                    r1,c1 = l
-                    if r1 in range(len(grid)) and c1 in range(len(grid[0])) and grid[r1][c1]==1 :
-                        grid[r1][c1] = 2
-                        count -= 1
-                        nq.append([r1,c1])    
-            for t in queue :
-                r,c = t
-                helper(r,c)
-
-            queue = nq
-        if count != 0:
-            return -1
-        return time
-        
-        
-        
                     
-            
-            
-            
+        if good == 0:
+            return 0
+        res = 0
+        while queue :
+            nq = []
+            for r,c in queue :
+                traverse = [(r-1,c),(r+1,c),(r,c-1),(r,c+1)]
+                for i,j in traverse :
+                    if i in range(len(grid)) and j in range(len(grid[0])) and grid[i][j] == 1 :
+                        good -= 1
+                        grid[i][j] = 2
+                        nq.append([i,j])
+            if nq :        
+                queue = nq
+                res += 1
+            else:
+                break
+        
+        if good == 0:
+            return res
+        return -1
+        
         
