cc325d6060cef81a371d00a653a672388bed43a8
Abhijeet Gupta <53190754+abhijeetgupto@users.noreply.github.com>
2021-10-30 19:28:48 +0530

    Time: 1099 ms (49.80%), Space: 57.2 MB (72.71%) - LeetHub

--- stock-price-fluctuation/stock-price-fluctuation.py
+++ stock-price-fluctuation/stock-price-fluctuation.py
@@ -1,35 +1,29 @@
+import sortedcontainers
+
 class StockPrice:
 
     def __init__(self):
         self.dic = {}
         self.time = -math.inf
-        self.l = []
+        self.l = sortedcontainers.SortedList()
 
     def update(self, timestamp: int, price: int) -> None:
         if timestamp in self.dic:
             self.l.remove(self.dic[timestamp])
-            
+
         if timestamp > self.time:
             self.time = timestamp
-            
+
         self.dic[timestamp] = price
-        bisect.insort(self.l,price)
-        
+        self.l.add(price)
+        # bisect.insort(self.l,price)
+
 
     def current(self) -> int:
         return self.dic[self.time]
-        
+
     def maximum(self) -> int:
         return self.l[-1]
 
     def minimum(self) -> int:
         return self.l[0]
-       
-
-
-# Your StockPrice object will be instantiated and called as such:
-# obj = StockPrice()
-# obj.update(timestamp,price)
-# param_2 = obj.current()
-# param_3 = obj.maximum()
-# param_4 = obj.minimum()
