7a48534c639615369a5ca8eb5f94180ad37f69be
Abhijeet Gupta <53190754+abhijeetgupto@users.noreply.github.com>
2022-01-04 11:57:48 +0530

    Time: 1788 ms (29.53%), Space: 43.5 MB (6.94%) - LeetHub

--- 322-coin-change/322-coin-change.py
+++ 322-coin-change/322-coin-change.py
@@ -1,39 +1,50 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         
-        dp = [0]*(amount+1)
-        for i in range(1, len(dp)) :
-            m = math.inf
-            for amt in coins :
-                if i-amt >= 0 and dp[i-amt] >= 0 :
-                    m = min(m,dp[i-amt])
-            if m != math.inf : 
-                dp[i] = 1+m
-            else:
-                dp[i] = -1
-        return dp[-1]
+#         #iterative
         
-
-#         def rec(arr, target,memo={}, depth = 0) :
-            
-#             if target in memo :
-#                 return memo[target]
-            
-#             elif target == 0 :
-#                 return 1
-            
-#             elif target < 0:
-#                 return 0
-            
+#         dp = [0]*(amount+1)
+#         for i in range(1, len(dp)) :
+#             m = math.inf
+#             for amt in coins :
+#                 if i-amt >= 0 and dp[i-amt] >= 0 :
+#                     m = min(m,dp[i-amt])
+#             if m != math.inf : 
+#                 dp[i] = 1+m
 #             else:
-#                 count = 0
-#                 for num in arr :
-#                     if rec(arr,target-num,memo) != 0 :
-#                         count += 1
-#                 memo[target] = count
-#                 return memo[target]
+#                 dp[i] = -1
+#         return dp[-1]
+        
+        
+        #recursive
+        
+        def rec(arr, target,memo={}, depth = 0) :
             
-#         return rec(coins, amount)
+            if target in memo :
+                return memo[target]
+            
+            elif target == 0 :
+                return 0
+            
+            elif target < 0:
+                return -1
+            
+            else:
+                m = math.inf
+                for num in arr :
+                    x = rec(arr,target-num,memo)
+                    if x >= 0 :
+                        m = min(m,x)
+                        
+                if m!=math.inf :
+                    memo[target] = 1+m
+                    
+                else:
+                    memo[target] = -1
+                    
+                return memo[target]
+            
+        return rec(coins, amount)
                     
                     
                         
