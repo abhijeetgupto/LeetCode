1ccaa5401a3ad6e8db0c4e5c9aec4ed495ea241a
Abhijeet Gupta <53190754+abhijeetgupto@users.noreply.github.com>
2022-01-05 14:21:50 +0530

    Time: 2416 ms (5.09%), Space: 27.9 MB (91.34%) - LeetHub

---
+++ 1696-jump-game-vi/1696-jump-game-vi.py
@@ -0,0 +1,35 @@
+import sortedcontainers
+
+class Solution:
+    def maxResult(self, nums: List[int], k: int) -> int:
+        
+        dp = [0]*(len(nums))
+        dp[0] = nums[0]
+        
+        m = dp[0]
+        l = sortedcontainers.SortedList()
+        l.add(dp[0])
+        
+        for i in range(1, min(k+1,len(nums))) :
+            dp[i] = nums[i] + l[-1]
+            l.add(dp[i])
+        
+        for i in range(k+1,len(dp)) :
+            l.remove(dp[i-k-1])
+            dp[i] = nums[i]+l[-1]
+            l.add(dp[i])
+        
+        return dp[-1]
+            
+            
+            
+            
+            
+            
+            
+            
+            
+        
+                        
+        
+        
