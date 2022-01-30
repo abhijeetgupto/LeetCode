115f53d12e2ba2bad7bafdf2d08261cff9c23f8b
Abhijeet Gupta <53190754+abhijeetgupto@users.noreply.github.com>
2022-01-30 11:51:22 +0530

    Time: 100 ms (76.36%), Space: 15.8 MB (37.21%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.py
+++ 64-minimum-path-sum/64-minimum-path-sum.py
@@ -3,23 +3,40 @@ class Solution:
         n = len(grid)
         m = len(grid[0])
         
-        #Recursive
-        memo = {}
-        def rec(i=0,j=0):
+        #Recursive............
+#         memo = {}
+#         def rec(i=0,j=0):
             
-            if (i,j) in memo :
-                return memo[(i,j)]
+#             if (i,j) in memo :
+#                 return memo[(i,j)]
             
-            if i==n-1 and j==m-1 :
-                return grid[i][j]
+#             if i==n-1 and j==m-1 :
+#                 return grid[i][j]
             
-            if i>=n or j>=m :
-                return math.inf
+#             if i>=n or j>=m :
+#                 return math.inf
             
-            memo[(i,j)] = grid[i][j] + min(rec(i+1,j), rec(i,j+1))
-            return memo[(i,j)]
+#             memo[(i,j)] = grid[i][j] + min(rec(i+1,j), rec(i,j+1))
+#             return memo[(i,j)]
         
-        return rec()
+#         return rec()
     
-        #Iterative 
+        #Iterative ...........
+        dp = [[0]*m for _ in range(n)]
+        curr = 0
+        for i in range(m) :
+            curr += grid[0][i]
+            dp[0][i] = curr
+            
+        curr = 0
+        for i in range(n) :
+            curr += grid[i][0]
+            dp[i][0] = curr
+
+        for i in range(1,n):
+            for j in range(1,m):
+                dp[i][j] = grid[i][j] + min(dp[i-1][j], dp[i][j-1])
+        
+        return dp[n-1][m-1]
+                
         
