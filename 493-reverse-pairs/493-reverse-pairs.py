ee78baff97b076f8980e48d848cb324fddca6dc6
Abhijeet Gupta <53190754+abhijeetgupto@users.noreply.github.com>
2022-02-10 10:25:07 +0530

    Time: 2080 ms (79.42%), Space: 21.3 MB (87.24%) - LeetHub

--- 493-reverse-pairs/493-reverse-pairs.py
+++ 493-reverse-pairs/493-reverse-pairs.py
@@ -1,15 +1,14 @@
-import sortedcontainers
 import bisect
 
 class Solution:
     def reversePairs(self, nums: List[int]) -> int:
         
-        temp = sortedcontainers.SortedList([])
+        temp = []
         count = 0
         
         for i in range(len(nums)) :
             
             count += len(temp)-bisect.bisect_right(temp, 2*nums[i])
-            temp.add(nums[i])
+            bisect.insort(temp, nums[i])
         
         return count
