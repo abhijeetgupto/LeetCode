8178544ae2a821d29e0b0998cb485bdda8652693
Abhijeet Gupta <53190754+abhijeetgupto@users.noreply.github.com>
2022-02-11 12:43:22 +0530

    Time: 1281 ms (5.04%), Space: 19.2 MB (99.38%) - LeetHub

---
+++ 881-boats-to-save-people/881-boats-to-save-people.py
@@ -0,0 +1,38 @@
+class Solution:
+    def numRescueBoats(self, people: List[int], limit: int) -> int:
+        
+        people.sort()
+        count = 0
+        
+        while people :
+            top = people.pop(0)
+            x = limit-top
+            if people : 
+                if x == 0 :
+                    count += 1
+
+                else:
+                    idx = bisect.bisect_left(people, x)
+
+                    if idx == len(people) :
+                        people.pop(idx-1)
+                        count += 1
+
+                    elif people[idx] == x :
+                        people.pop(idx)
+                        count += 1
+
+                    else:
+                        if idx == 0 :
+                            count += 1
+                        else:
+                            people.pop(idx-1)
+                            count += 1
+            else:
+                count+=1
+        return count
+                        
+                
+            
+        
+        
