86e5bdac7b61ed4ffb10a357d34f89dc070367cf
Abhijeet Gupta <53190754+abhijeetgupto@users.noreply.github.com>
2022-03-05 12:11:25 +0530

    Time: 110 ms (25.32%), Space: 14.3 MB (66.35%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.py
+++ 740-delete-and-earn/740-delete-and-earn.py
@@ -1,26 +1,46 @@
 class Solution:
     def deleteAndEarn(self, nums: List[int]) -> int:
-
-        dic = dict(collections.Counter(nums))
-        arr = sorted(dic.keys())
-        n = len(arr)
         
-        taken = arr[n-1]*dic[arr[n-1]]
-        not_taken = 0
         
-        for i in range(n-2,-1,-1) :
+        def helper(nums) :
             
-            if arr[i]+1 == arr[i+1] :
-                temp = not_taken + arr[i]*dic[arr[i]]
-                not_taken = max(taken, not_taken)
-                taken = temp
-                
-            else:
-                temp = max(taken, not_taken) + arr[i]*dic[arr[i]]
-                not_taken = max(taken, not_taken)
-                taken = temp
+            if len(nums) <=  2 :
+                return max(nums)
         
-        return max(taken, not_taken)
+            if len(nums) == 3 :
+                return max(nums[1],nums[0]+nums[2])
+
+            l = [0]*len(nums)
+            l[0],l[1],l[2] = nums[0],nums[1],max(nums[2]+nums[0],nums[1])
+
+
+            for i in range(3, len(l)):
+                l[i] = max(l[i-1], nums[i]+l[i-2], nums[i]+l[i-3])
+
+            return l[-1]
+
+        dic = collections.Counter(nums)
+        keys = list(dic.keys())
+        keys.sort()
+        
+        res = []
+        last = keys.pop(0)
+        temp = [last*dic[last]]
+        while keys :
+            top = keys.pop(0)
+            if top == last+1 :
+                temp.append(top*dic[top])
+                last = top
+            else:
+                res.append(temp)
+                temp = [top*dic[top]]
+                last = top
+        res.append(temp)
+    
+        count = 0
+        for l in res:
+            count += helper(l)
+        return count
             
         
             
