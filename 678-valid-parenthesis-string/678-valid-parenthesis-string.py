5c9b681b0ec6dded8a1d58017de3ab2935f5153e
Abhijeet Gupta <53190754+abhijeetgupto@users.noreply.github.com>
2022-04-07 12:09:34 +0530

    Time: 177 ms (5.13%), Space: 13.9 MB (26.96%) - LeetHub

--- 678-valid-parenthesis-string/678-valid-parenthesis-string.py
+++ 678-valid-parenthesis-string/678-valid-parenthesis-string.py
@@ -3,39 +3,30 @@ class Solution:
         
         n = len(s)
         
-        memo = {}
         def rec(i=0, k=0, star = 0) :
-            
-            if (i,k,star) in memo:
-                return memo[(i,k,star)]
             
             if i>=n:
                 return k==0
             
             if s[i] == ")" :
                 if k>0:
-                    memo[(i,k,star)] = rec(i+1,k-1, star)
-                    return memo[(i,k,star)]
+                    return rec(i+1,k-1, star)
                 
                 elif star > 0:
-                    memo[(i,k,star)] = rec(i+1,k, star-1)
-                    return memo[(i,k,star)]
+                    return rec(i+1,k, star-1)
                 
                 else:
                     return False
             
             elif s[i] == "(" :
                 
-                memo[(i,k,star)] = rec(i+1,k+1,star)
-                return memo[(i,k,star)]
+                return rec(i+1,k+1,star)
         
             else:
                 if k>0:
-                    memo[(i,k,star)]= rec(i+1, k-1, star) or rec(i+1,k,star+1)
-                    return memo[(i,k,star)]
+                    return rec(i+1, k-1, star) or rec(i+1,k,star+1)
                 
                 else:
-                    memo[(i,k,star)] = rec(i+1,k,star+1)
-                    return memo[(i,k,star)]
+                    return rec(i+1,k,star+1)
         return rec()
         
