5cfe918f8c617fcbad9544d14740fa98a9b504a7
Abhijeet Gupta <53190754+abhijeetgupto@users.noreply.github.com>
2022-04-26 16:09:27 +0530

    Time: 9507 ms (5.00%), Space: 81.6 MB (69.64%) - LeetHub

---
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
@@ -0,0 +1,35 @@
+class Solution:
+    def minCostConnectPoints(self, points: List[List[int]]) -> int:
+        
+        parent = list(range(len(points)))
+        
+        def find(x):
+            if parent[x] == x :
+                return x
+            return find(parent[x])
+        
+        
+        
+        graph = []
+        for i in range(len(points)):
+            x1,y1 = points[i][0],points[i][1]
+            for j in range(i+1, len(points)):
+                x2,y2 = points[j][0],points[j][1]
+                graph.append(((abs(x1-x2) + abs(y1-y2)),i,j))
+                
+        graph.sort()
+        count = 0
+        
+        for w,i,j in graph :
+            x1 = find(i)
+            x2 = find(j)
+            if x1 != x2 :
+                parent[x1] = x2
+                count += w
+                
+        return count
+                
+            
+            
+        
+        
