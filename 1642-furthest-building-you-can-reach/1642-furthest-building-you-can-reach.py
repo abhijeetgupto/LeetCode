8930720c8dcda0126aba3c9e66d75f957b560cbb
Abhijeet Gupta <53190754+abhijeetgupto@users.noreply.github.com>
2022-05-08 17:06:37 +0530

    Time: 803 ms (41.05%), Space: 28.4 MB (92.53%) - LeetHub

--- 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.py
+++ 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.py
@@ -1,68 +1,17 @@
-from sortedcontainers import SortedList
-
+import heapq
 class Solution:
     def furthestBuilding(self, heights: List[int], bricks: int, ladders: int) -> int:
         
-        if ladders == 0:
-            
-            for i in range(1, len(heights)):
-                if heights[i]-heights[i-1]>0:
-                    bricks -= heights[i]-heights[i-1]
-                
-                if bricks < 0 :
-                    return i-1
-            return i
-                
-            
+        heap = []
+        heapq.heapify(heap)
         
-        temp = []
         for i in range(1, len(heights)):
-            x = heights[i] - heights[i - 1]
-            if x > 0:
-                temp.append([x, i])
-        
-        if not temp or ladders >= len(temp) :
-            return len(heights)-1
-        
-
-        ladders_arr = SortedList()
-        for i in range(ladders):
-            ladders_arr.add(temp[i][0])
-        
-
-        bricks_arr = SortedList()
-        for i in range(ladders, len(temp)):
-            x = temp[i][0]
-            if x < ladders_arr[0]:
-                bricks_arr.add(x)
-                bricks -= x
-            else:
-                t = ladders_arr[0]
-                bricks_arr.add(t)
-                ladders_arr.remove(t)
-                ladders_arr.add(x)
-                bricks -= t
-            if bricks == 0:
-                break
-            elif bricks < 0:
-                i -= 1
-                break
-
-        idx = temp[i][1]
-        for i in range(idx+1, len(heights)):
-            if heights[i] <= heights[i-1]:
-                idx+=1
-            else:
-                break
+            if heights[i] - heights[i-1] > 0 :
+                heapq.heappush(heap, heights[i]-heights[i-1])
                 
-        return idx
-        
-        
-  
-
-        
-        
-            
-                
-            
+                if len(heap) > ladders :
+                    bricks -= heapq.heappop(heap)
+            if bricks < 0:
+                return i-1
+        return i
         
