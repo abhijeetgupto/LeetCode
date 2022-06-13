af90dcc3481d7c961d3f3f3a4bad36e7c8ccd00c
Abhijeet Gupta <53190754+abhijeetgupto@users.noreply.github.com>
2022-06-13 18:34:22 +0530

    Time: 1769 ms (12.50%), Space: 13.8 MB (87.50%) - LeetHub

--- 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.py
+++ 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.py
@@ -4,18 +4,18 @@ class Solution:
         n = len(cookies)
         res = [math.inf]
         
-        def rec(i=0, curr=[0]*k):
+        def rec(i=0, curr=[0]*k, curr_max = 0):
             
             if i == n:
-                res[0] = min(res[0], max(curr))
+                res[0] = min(res[0], curr_max)
                 return 
             
-            if max(curr) > res[0]:
+            if curr_max > res[0]:
                 return 
             
             for child in range(k):
                 curr[child] += cookies[i]
-                rec(i+1, curr)
+                rec(i+1, curr, max(curr_max, curr[child]))
                 curr[child] -= cookies[i]
                 
             return 
