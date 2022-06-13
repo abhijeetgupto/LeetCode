a6073e8541af881acb9bfe53168ac4fb69bff9cc
Abhijeet Gupta <53190754+abhijeetgupto@users.noreply.github.com>
2022-06-13 16:03:22 +0530

    Time: 63 ms (97.61%), Space: 17.3 MB (7.66%) - LeetHub

--- 120-triangle/120-triangle.py
+++ 120-triangle/120-triangle.py
@@ -1,21 +1,19 @@
+from functools import lru_cache
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
         
         n = len(triangle)
         
-        memo = {}
-        def rec(i=0, j=0) :
+        @lru_cache(None)
+        def rec(i=0, j=0):
             
-            if (i,j) in memo:
-                return memo[(i,j)]
-            
-            if i==n-1 :
+            if i==n-1:
                 return triangle[i][j]
             
-            memo[(i,j)] = triangle[i][j] + min(rec(i+1,j),rec(i+1,j+1))
-            return memo[(i,j)]
+            else:
+                return triangle[i][j] + min(rec(i+1,j), rec(i+1,j+1))
         
         return rec()
-            
+                
             
         
