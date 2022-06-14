9f5dc311d8649fc52b8b16e003ab4601739d047f
Abhijeet Gupta <53190754+abhijeetgupto@users.noreply.github.com>
2022-06-14 10:21:55 +0530

    Time: 611 ms (11.12%), Space: 32.7 MB (7.00%) - LeetHub

--- 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.py
+++ 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.py
@@ -1,32 +1,27 @@
+from functools import lru_cache    
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
         
-        def rec(s1, s2, i, j, memo={}):
-
-            if (i, j) in memo:
-                return memo[(i, j)]
-
-            elif i < len(s1) and j < len(s2):
-                
-                if s1[i] == s2[j]:
-                    memo[(i,j)] = rec(s1, s2, i + 1, j + 1, memo)
-                    return memo[(i,j)]
-
-                else:
-                    x1 = 1 + rec(s1, s2, i + 1, j, memo)
-                    x2 = 1 + rec(s1, s2, i, j + 1, memo)
-                    x3 = 2 + rec(s1, s2, i + 1, j + 1, memo)
-                    memo[(i, j)] = min(x1, x2, x3)
-                    return memo[(i, j)]
-                
+        n = len(word1)
+        m = len(word2)
+        
+        @lru_cache(None)
+        def rec(i=0, j=0):
+            
+            if i==n :
+                return m-j
+            
+            elif j==m:
+                return n-i
+            
+            if word1[i] == word2[j]:
+                return rec(i+1, j+1)
+            
             else:
-                if i >= len(s1) and j >= len(s2):
-                    return 0
-
-                elif i >= len(s1):
-                    return len(s2) - j
-
-                else:
-                    return len(s1) - i
-
-        return rec(word1, word2, 0,0)
+                x = 1 + rec(i+1, j)
+                y = 1 + rec(i, j+1)
+                return min(x,y)
+        
+        return rec()
+        
+            
