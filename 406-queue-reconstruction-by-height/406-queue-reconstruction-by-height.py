6631aefdea779b27cbe31cb3fad383ed990e7114
Abhijeet Gupta <53190754+abhijeetgupto@users.noreply.github.com>
2022-06-29 14:07:39 +0530

    Time: 123 ms (74.40%), Space: 14.9 MB (6.40%) - LeetHub

--- 406-queue-reconstruction-by-height/406-queue-reconstruction-by-height.py
+++ 406-queue-reconstruction-by-height/406-queue-reconstruction-by-height.py
@@ -3,10 +3,13 @@ from sortedcontainers import SortedList
 class Solution:
     def reconstructQueue(self, people: List[List[int]]) -> List[List[int]]:
         
+
         n = len(people)
         temp = list(range(n))
         res = [0]*n
         people = sorted(people, key=lambda x: (x[0], -x[1]))
+        # print(people)
+        
         for i in range(n):
             idx = temp.pop(people[i][1])
             res[idx] = people[i]
