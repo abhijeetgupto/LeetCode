ac6d67fdea9be5c108bb356cab90b93db4bb6d10
Abhijeet Gupta <53190754+abhijeetgupto@users.noreply.github.com>
2022-07-03 10:39:39 +0530

    Time: 1176 ms (5.03%), Space: 174.4 MB (5.22%) - LeetHub

--- 376-wiggle-subsequence/376-wiggle-subsequence.py
+++ 376-wiggle-subsequence/376-wiggle-subsequence.py
@@ -2,29 +2,26 @@ class Solution:
     def wiggleMaxLength(self, nums: List[int]) -> int:
         
         n = len(nums)
-        memo = {}
         
+        @cache        
         def rec(i, j, inc) :
-            
-            if (i,j,inc) in memo :
-                return memo[(i,j,inc)]
+
             
             if i >= n :
                 return 0
             
             if inc :
                 if nums[i] > nums[j] :
-                    memo[(i,j,inc)] = max(1+rec(i+1, i, False), rec(i+1, j, True))
+                    return max(1+rec(i+1, i, False), rec(i+1, j, True))
                 else:
-                    memo[(i,j,inc)] =  rec(i+1, j, True)
+                    return  rec(i+1, j, True)
             
             else:
                 if nums[i] < nums[j] :
-                    memo[(i,j,inc)] = max(1+rec(i+1, i , True), rec(i+1, j, False))
+                    return  max(1+rec(i+1, i , True), rec(i+1, j, False))
                 else:
-                    memo[(i,j,inc)] = rec(i+1, j, False)
-                 
-            return memo[(i,j,inc)]
+                    return rec(i+1, j, False)
+
         
         res = 0
         for i in range(n):
