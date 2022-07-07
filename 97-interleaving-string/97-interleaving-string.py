574457dbad15e60ed6bb3717623bb4c50031a1d8
Abhijeet Gupta <53190754+abhijeetgupto@users.noreply.github.com>
2022-07-07 10:20:08 +0530

    Time: 57 ms (67.04%), Space: 15 MB (38.00%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.py
+++ 97-interleaving-string/97-interleaving-string.py
@@ -1,14 +1,13 @@
 class Solution:
     def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
         
-        def rec(s1, s2, s3, i, j, k, memo={}) :
-            
-            n1 = len(s1)
-            n2 = len(s2)
-            n3 = len(s3)
-            
-            if (i,j,k) in memo :
-                return memo[(i,j,k)]
+        n1 = len(s1)
+        n2 = len(s2)
+        n3 = len(s3)
+        
+        @lru_cache(None)
+        def rec(i, j, k) :
+
             
             if i >= n1 and j >= n2 and k >= n3 :
                 return True
@@ -34,20 +33,19 @@ class Solution:
             else:
                 
                 if s1[i] == s3[k] and s2[j] == s3[k] :
-                    memo[(i,j,k)] = rec(s1,s2,s3,i+1,j,k+1) or  rec(s1,s2,s3,i,j+1,k+1)
-                    return memo[(i,j,k)]
+                    return  rec(i+1,j,k+1) or  rec(i,j+1,k+1)
+                    
                 
                 elif s1[i] == s3[k] :
-                    memo[(i,j,k)] = rec(s1,s2,s3,i+1,j,k+1)
-                    return memo[(i,j,k)]
+                    return  rec(i+1,j,k+1)
+                   
                 
                 elif s2[j] == s3[k] :
-                    memo[(i,j,k)] = rec(s1,s2,s3,i,j+1,k+1)
-                    return memo[(i,j,k)]
-                
+                    return rec(i,j+1,k+1)
+                    
                 else:
                     return False
                 
-        return rec(s1,s2,s3,0,0,0)
+        return rec(0,0,0)
                 
             
