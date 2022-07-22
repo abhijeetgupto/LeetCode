2071b5baae6be08ed022c781e7bdd650a687e177
Abhijeet Gupta <53190754+abhijeetgupto@users.noreply.github.com>
2022-07-22 13:47:00 +0530

    Time: 104 ms (74.44%), Space: 13.9 MB (77.49%) - LeetHub

--- 1079-letter-tile-possibilities/1079-letter-tile-possibilities.py
+++ 1079-letter-tile-possibilities/1079-letter-tile-possibilities.py
@@ -1,20 +1,37 @@
 class Solution:
     def numTilePossibilities(self, tiles: str) -> int:
         
-        s = set()
-        n = len(tiles)
+        dic = dict(Counter(tiles))
         
-        def rec(curr = ""):
+        def rec():
             
-            s.add(curr)
-            for i in range(n):
-                if not visited[i]:
-                    visited[i] = True
-                    rec(curr+tiles[i])
-                    visited[i] = False
+            res = 0
+            for key in dic:
+                if dic[key] != 0:
+                    dic[key] -= 1
+                    res += 1 + rec()
+                    dic[key] += 1
+            return res
+        
+        return rec()
+            
+        
+
+        
+#         s = set()
+#         n = len(tiles)
+        
+#         def rec(curr = ""):
+            
+#             s.add(curr)
+#             for i in range(n):
+#                 if not visited[i]:
+#                     visited[i] = True
+#                     rec(curr+tiles[i])
+#                     visited[i] = False
                 
 
-        visited = [False]*n
-        rec()
-        return len(s)-1
+#         visited = [False]*n
+#         rec()
+#         return len(s)-1
         
