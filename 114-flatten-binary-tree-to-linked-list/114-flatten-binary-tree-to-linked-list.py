2b745ef8fb648ba269d8fd432ea318000a2dc39c
Abhijeet Gupta <53190754+abhijeetgupto@users.noreply.github.com>
2022-07-27 09:11:40 +0530

    Time: 60 ms (46.91%), Space: 15.3 MB (10.29%) - LeetHub

--- 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.py
+++ 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.py
@@ -6,31 +6,54 @@
 #         self.right = right
 class Solution:
     def flatten(self, root: Optional[TreeNode]) -> None:
-        if not root :
-            return 
         
-        def preorder(node):
+        def rec(node = root):
             
             if not node:
                 return 
             
-            order.append(node.val)
-            preorder(node.left)
-            preorder(node.right)
-        
-        order = []
-        preorder(root)
+            if node.left:
 
-        curr = root     
-        n = len(order)
-        for i in range(len(order)):
-            curr.val = order[i]
-            curr.left = None
-            if i<n-1 and not curr.right:
-                curr.right = TreeNode(0)
-            curr = curr.right
+                temp = node.right
+                rightmost = node.left
+                while rightmost.right:
+                    rightmost = rightmost.right
+                
+                node.right = node.left
+                rightmost.right = temp
+                node.left = None
+                
+            if node.right:
+                rec(node.right)
             
-        return 
+        rec()
+        
+        
+#         if not root :
+#             return 
+        
+#         def preorder(node):
+            
+#             if not node:
+#                 return 
+            
+#             order.append(node.val)
+#             preorder(node.left)
+#             preorder(node.right)
+        
+#         order = []
+#         preorder(root)
+
+#         curr = root     
+#         n = len(order)
+#         for i in range(len(order)):
+#             curr.val = order[i]
+#             curr.left = None
+#             if i<n-1 and not curr.right:
+#                 curr.right = TreeNode(0)
+#             curr = curr.right
+            
+#         return 
         
         
             
