dc20f26b649c3ffd3a3812e1de214bfd2df783cf
Abhijeet Gupta <53190754+abhijeetgupto@users.noreply.github.com>
2022-07-27 08:50:51 +0530

    Time: 66 ms (33.37%), Space: 15.2 MB (47.67%) - LeetHub

---
+++ 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.py
@@ -0,0 +1,37 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def flatten(self, root: Optional[TreeNode]) -> None:
+        if not root :
+            return 
+        
+        def preorder(node):
+            
+            if not node:
+                return 
+            
+            order.append(node.val)
+            preorder(node.left)
+            preorder(node.right)
+        
+        order = []
+        preorder(root)
+
+        curr = root     
+        n = len(order)
+        for i in range(len(order)):
+            curr.val = order[i]
+            curr.left = None
+            if i<n-1 and not curr.right:
+                curr.right = TreeNode(0)
+            curr = curr.right
+            
+        return 
+        
+        
+            
+        
